9198a1994ff1c28269a8a5d03158cf7793268f7a
Jacopo <jacopocascioli@gmail.com>
2018-09-09 21:44:16 +0100

    chore(setup): do not distribute wheel

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ from sqlast.Version import version
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
-    os.system('python setup.py bdist_wheel upload')
     sys.exit()
 
 
